4853006293d6a5ea0cb3499361916aa56dc1819a
Massa2201 <chrono.trigger.2424@gmail.com>
2020-11-07 19:12:30 +0900

    ログイン機能の追加およびメイン画面作成

--- WebApp.php
+++ WebApp.php
@@ -1,12 +1,34 @@
 <?php
 
+$passlist = array('member' => 'memberpass');
+
+
+if (!isset($_POST['user'])) {
+    login("ログインしてください");
+    exit;
+}
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+
+if ((!isset($passlist[$user])) || $passlist[$user] != $pass) {
+    login("パスワードが存在しないか違っています");
+    exit;
+} else {
+    main();
+    exit;
+}
 if (isset($_POST['trans'])) {
 
     if ($_POST['trans'] == "login") {
-        login();
+        login("");
+        exit;
+    } else if ($_POST['trans'] == "main") {
+        main();
+        exit;
     }
 } else {
-    main();
+    login("");
+    exit;
 }
 
 function main()
@@ -23,7 +45,7 @@ function main()
 
 <body>
 
-    <h1>勤務管理システム</h1>
+    <h1>勤務管理</h1>
 
     
 
@@ -35,9 +57,35 @@ function main()
 EOT;
 }
 
+function login($text)
+{
 
-?>
+    global $user, $pass;
 
-</body>
+    echo <<<EOT
+    <!DOCTYPE html>
+    <html>
+    
+    <head>
+        <meta charset="utf-8" />
+        <title>メイン画面</title>
+    </head>
+    
+    <body>
+    
+        <h1>勤務管理システム</h1>
+        <p>$text</p><br>
+        <form method="POST" action="WebApp.php">
+        username <input type="text" name="user" value=""><br>
+        password <input type="password" name="pass" value=""><br>
+        <form method="POST" action="WebApp.php">
+            <button type="submit" name="btn01" value="btn01">ログイン</button>
+            <input type="hidden" name="trans" value="main">
+    
+    
+    
+    </body>
+    </html>
 
-</html>
+EOT;
+}
