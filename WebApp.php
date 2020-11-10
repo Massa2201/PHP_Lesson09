a2edd520d70aa74149b7d351f2c6e3032484d72f
Massa2201 <chrono.trigger.2424@gmail.com>
2020-11-10 23:00:57 +0900

    ログインと出勤ボタンの作成

--- WebApp.php
+++ WebApp.php
@@ -14,7 +14,7 @@ if ((!isset($passlist[$user])) || $passlist[$user] != $pass) {
     login("パスワードが存在しないか違っています");
     exit;
 } else {
-    main();
+    start();
     exit;
 }
 if (isset($_POST['trans'])) {
@@ -22,8 +22,11 @@ if (isset($_POST['trans'])) {
     if ($_POST['trans'] == "login") {
         login("");
         exit;
-    } else if ($_POST['trans'] == "main") {
-        main();
+    } else if ($_POST['trans'] == "start") {
+        start();
+        exit;
+    } else if ($_POST['trans'] == "finish") {
+        finish();
         exit;
     }
 } else {
@@ -31,7 +34,7 @@ if (isset($_POST['trans'])) {
     exit;
 }
 
-function main()
+function start()
 {
 
     echo <<<EOT
@@ -45,10 +48,27 @@ function main()
 
 <body>
 
-    <h1>勤務管理</h1>
+    <header>
 
+    <h1>メイン画面</h1>
+
+    </header>
+
+    <main>
+
+    <p>2020年11月07日(土)</p>
+    <p>20:00</p>
     
+    <form method="POST" action="WebApp.php">
+        <button type="submit" name="btn02" value="btn02">出勤</button>
+        <input type="hidden" name="trans" value="finish">
+    </form>
 
+    </main>
+
+    <footer>
+
+    </footer>
 
 
 </body>
@@ -76,16 +96,50 @@ function login($text)
         <h1>勤務管理システム</h1>
         <p>$text</p><br>
         <form method="POST" action="WebApp.php">
-        username <input type="text" name="user" value=""><br>
-        password <input type="password" name="pass" value=""><br>
-        <form method="POST" action="WebApp.php">
+            username <input type="text" name="user" value=""><br>
+            password <input type="password" name="pass" value=""><br>
             <button type="submit" name="btn01" value="btn01">ログイン</button>
-            <input type="hidden" name="trans" value="main">
+            <input type="hidden" name="trans" value="start">
+        </form>
     
     
     
     </body>
     </html>
+
+EOT;
+}
+
+function finish()
+{
+    global $user, $pass;
+
+    echo <<<EOT
+
+    <!DOCTYPE html>
+    <html>
+    
+    <head>
+        <meta charset="utf-8" />
+        <title>トップ画面</title>
+    </head>
+    
+    <body>
+    
+        <h1>トップ画面</h1>
+        <p>finish</p><br>
+        <form method="POST" action="WebApp.php">
+            username <input type="text" name="user" value=""><br>
+            password <input type="password" name="pass" value=""><br>
+            <button type="submit" name="btn01" value="btn01">ログイン</button>
+            <input type="hidden" name="trans" value="start">
+        </form>
+    
+    
+    
+    </body>
+    </html>
+
 
 EOT;
 }
