0762264c5a1ac999d76359ffd0546499626b066c
Massa2201 <chrono.trigger.2424@gmail.com>
2020-11-20 20:39:05 +0900

    ソースコード，beauty使用

--- Lesson11-1.php
+++ Lesson11-1.php
@@ -1,55 +1,56 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8" />
-        <title>メイン画面</title>
-    </head>
-    <body>
 
-<?php
-$passlist=array( 'hogehoge' => 'hogepass', 'hoge2' => 'hoge2pass');
+<head>
+    <meta charset="UTF-8" />
+    <title>メイン画面</title>
+</head>
 
-if(!isset($_POST['user']))
-{
-    echo_auth_page("ログインしてください");
-    exit;
-}
-$user=$_POST['user'];
-$pass=$_POST['pass'];
+<body>
 
-if( (!isset($passlist[$user])) || $passlist[$user] != $pass)
-{
-    echo_auth_page("パスワードが違います");
-    exit;
-}
+    <?php
+    $passlist = array('hogehoge' => 'hogepass', 'hoge2' => 'hoge2pass');
 
-if(isset($_POST['transition']))
-{
-    next_page("遷移に成功しました");
-    exit;
-}
+    if (!isset($_POST['user'])) {
+        echo_auth_page("ログインしてください");
+        exit;
+    }
+    $user = $_POST['user'];
+    $pass = $_POST['pass'];
 
-echo_hello_page($user);
+    if ((!isset($passlist[$user])) || $passlist[$user] != $pass) {
+        echo_auth_page("パスワードが違います");
+        exit;
+    }
+
+    if (isset($_POST['transition'])) {
+        next_page("遷移に成功しました");
+        exit;
+    }
+
+    echo_hello_page($user);
 
 
-////////////////////////////////////////////////////////////////////////
-function echo_auth_page($msg)
-{
-    echo <<<EOT
+    ////////////////////////////////////////////////////////////////////////
+    function echo_auth_page($msg)
+    {
+        echo <<<EOT
     $msg
     <form method="POST" action="Lesson11-1.php">
         username <input type="text" name="user" value=""><br>
         password <input type="password" name="pass" value=""><br>
         <button type="submit" name="login" value="login">Login</button>
     </form>
-EOT;
-}
-////////////////////////////////////////////////////////////////////////
-function echo_hello_page($who)
-{
-    global $user, $pass;
 
-echo <<<EOT
+    
+EOT;
+    }
+    ////////////////////////////////////////////////////////////////////////
+    function echo_hello_page($who)
+    {
+        global $user, $pass;
+
+        echo <<<EOT
 
 こんにちは $who さん
     <form method="POST" action="Lesson11-1.php">
@@ -59,13 +60,13 @@ echo <<<EOT
         <input type="hidden" name="pass" value="$pass">
     </form>
 EOT;
-}
+    }
 
-function next_page($text)
-{
-    global $user, $pass;
+    function next_page($text)
+    {
+        global $user, $pass;
 
-echo <<<EOT
+        echo <<<EOT
 
 $text
     <form method="POST" action="Lesson11-1.php">
@@ -73,8 +74,9 @@ $text
         <input type="hidden" name="user" value="$user">
         <input type="hidden" name="pass" value="$pass">
 EOT;
-}
+    }
 
-?>
+    ?>
 </body>
+
 </html>
